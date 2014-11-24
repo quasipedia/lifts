3780b16df0607f055d1726805073bd3f5f6ad8ae
Mac Ryan <quasipedia@gmail.com>
2014-11-24 22:04:21 +0100

    Change enum properties

--- lifts/enums.py
+++ lifts/enums.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
-LiftStatus = Enum('LiftStatus', 'moving open closed')
-PersonStatus = Enum('PersonStatus', 'idle moving done')
 Dir = Enum('Dirs', 'up down')
 Event = Enum('Event', 'call_button floor_button')
+LiftStatus = Enum('LiftStatus', 'moving open closed')
+PersonStatus = Enum('PersonStatus', 'idle moving done')
 Command = Enum('Command', 'move open close')
 Message = Enum('Message', 'started ended lift_call floor_request error')
