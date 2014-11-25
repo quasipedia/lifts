43cc218cb4924c38571fd2baf9bc62c22ad535d2
Mac Ryan <quasipedia@gmail.com>
2014-11-25 14:02:42 +0100

    Stub of asyncio-based engine simulation

---
+++ test/test_engine.py
@@ -0,0 +1,28 @@
+'''Test suite for the engine module.'''
+
+import unittest
+from lifts import engine
+
+
+class TestEntityRegistry(unittest.TestCase):
+
+    '''Test suite for the EntityRegistry class'''
+
+    def test_(self):
+        pass
+
+
+class TestSimObject(unittest.TestCase):
+
+    '''Test suite for the SimObject class'''
+
+    def test_(self):
+        pass
+
+
+class Simulation(unittest.TestCase):
+
+    '''Test suite for the SimObject class'''
+
+    def test_(self):
+        pass
